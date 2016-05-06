761950b7f81ce9c0a85bc0124d125b566f87548f
Ritesh Shrestha <riteshshr1992@gmail.com>
2016-05-06 15:43:00 -0400

    Copied old code to new method

--- src/Customer.hpp
+++ src/Customer.hpp
@@ -1,38 +1,128 @@
 /*
-  Customer.hpp
+  Customer.cpp
 
-  Include file for Customer class
+  Definition file for Customer class
 */
 
-#ifndef CUSTOMER_H
-#define CUSTOMER_H
+#include "Customer.hpp"
+#include "Video.hpp"
 
-#include "Rental.hpp"
-
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
-class Customer {
-public:
+// constructor
+Customer::Customer(const std::string& name)
+    : name(name)
+{ }
 
-    // constructor
-    Customer(const std::string& name);
+// customer name
+std::string Customer::getName() const {
+
+    return name;
+}
+
+// add a rental to this customer
+void Customer::addRental(const Rental& rental) {
+
+    rentals.push_back(rental);
+}
+
+// calculates amount for current statement
+double Customer::amountFor(const Rental& r) const {
+
+    double thisAmount = 0;
+    switch(r.getVideo().getCode()) {
+
+        case Video::REGULAR:
+        thisAmount += 2;
+        if (r.getDaysRented() > 2)
+            thisAmount += (r.getDaysRented() - 2) * 1.5;
+        break;
+
+        case Video::NEW_RELEASE:
+        thisAmount += r.getDaysRented() * 3;
+        break;
+
+        case Video::CHILDRENS:
+        thisAmount += 1.5;
+        if (r.getDaysRented() > 3)
+            thisAmount += (r.getDaysRented() - 3) * 1.5;
+        break;
+    }
+
+    return 0;
+}
+
+// customer rental statement
+std::string Customer::statement() const {
 
     // customer name
-    std::string getName() const;
+    std::string result("Rental Record for ");
+    result += getName();
+    result += "\n";
 
-    // add a new rental
-    void addRental(const Rental& rental);
+    // rentals
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    for (std::vector<Rental>::const_iterator it = rentals.begin(); it != rentals.end(); ++it) {
 
-    // customer rental statement
-    std::string statement() const;
+        // every rental is a rental point
+        ++frequentRenterPoints;
 
-    // calculates amount for current statement
-    double amountFor() const;
+        // new releases rented for more then one day gives a bonus rental point
+        if (it->getVideo().getCode() == Video::NEW_RELEASE &&
+            it->getDaysRented() > 1 )
+            ++frequentRenterPoints;
 
-private:
-    std::string name;
-    std::vector<Rental> rentals;
-};
+        // title of rental
+        result += "\t";
+        result += it->getVideo().getTitle();
+        result += "\t";
 
-#endif
+        double thisAmount = 0;
+        switch(it->getVideo().getCode()) {
+
+            case Video::REGULAR:
+            thisAmount += 2;
+            if (it->getDaysRented() > 2)
+                thisAmount += (it->getDaysRented() - 2) * 1.5;
+            break;
+
+            case Video::NEW_RELEASE:
+            thisAmount += it->getDaysRented() * 3;
+            break;
+
+            case Video::CHILDRENS:
+            thisAmount += 1.5;
+            if (it->getDaysRented() > 3)
+                thisAmount += (it->getDaysRented() - 3) * 1.5;
+            break;
+        }
+
+        // amount of rental
+        std::ostringstream out_str_stream;
+        out_str_stream << thisAmount;
+        result += out_str_stream.str();
+        result += "\n";
+
+        totalAmount += thisAmount;
+    }
+
+    // total amount owed
+    result += "Amount owed is: ";
+    std::ostringstream out_str_stream;
+    out_str_stream << totalAmount;
+    result += out_str_stream.str();
+    result += "\n";
+
+    // frequent renter points earned
+    result += "You earned: ";
+    std::ostringstream out_str_stream2;
+    out_str_stream2 << frequentRenterPoints;
+    result += out_str_stream2.str();
+    result += " frequent renter points\n";
+
+    return result;
+}
