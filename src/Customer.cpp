c65826474b18e3fa4ec0b25b9734cfa5442f0f43
Ritesh Shrestha <riteshshr1992@gmail.com>
2016-05-09 21:37:29 -0400

    delete old code

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -82,24 +82,6 @@ std::string Customer::statement() const {
         result += "\t";
 
         double thisAmount = 0;
-        switch(it->getVideo().getCode()) {
-
-            case Video::REGULAR:
-            thisAmount += 2;
-            if (it->getDaysRented() > 2)
-                thisAmount += (it->getDaysRented() - 2) * 1.5;
-            break;
-
-            case Video::NEW_RELEASE:
-            thisAmount += it->getDaysRented() * 3;
-            break;
-
-            case Video::CHILDRENS:
-            thisAmount += 1.5;
-            if (it->getDaysRented() > 3)
-                thisAmount += (it->getDaysRented() - 3) * 1.5;
-            break;
-        }
         thisAmount = amountFor(*it);
 
         // amount of rental
